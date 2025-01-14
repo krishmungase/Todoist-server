ca7ebe65a8c7b70bdcc6d7a1e686a66bb1fd02e5
krishmungase <mungasekrishna8@gmail.com>
2025-01-15 00:18:17 +0530

    add cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const Config = require("./config");
 const database = require("./config/database");
 const todoRoutes = require("./routes/todo")
+const cors = require("cors");
 const app = express();
 
 
 
 
 database.connect();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
 
 app.use(express.json());
 app.use("/api/v1/todo", todoRoutes);
