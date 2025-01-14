c82e409c20ea2f0c20646c33457fff6006ed4155
krishmungase <mungasekrishna8@gmail.com>
2025-01-14 22:31:43 +0530

    create server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Config = require("./config");
+const database = require("./config/database");
+const todoRoutes = require("./routes/todo")
+const app = express();
+
+
+
+
+database.connect();
+
+app.use(express.json());
+app.use("/api/v1/todo", todoRoutes);
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: 'Your server is up and running....'
+  });
+});
+
+console.log("PORT: ", Config.PORT)
+
+app.listen(Config.PORT, () => {
+  console.log(`App is running at ${Config.PORT}`)
+})
